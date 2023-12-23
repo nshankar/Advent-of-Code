93bbe509fd1ea1ac9572db1b540d069bea56329d
Nikhil Shankar <nshankar@umich.edu>
2023-12-22 16:23:12 -0800

    Finish Day 17 part 2

--- 2023/17/second_part.py
+++ 2023/17/second_part.py
@@ -9,8 +9,8 @@ def initialize_graph(data):
     in_bounds, v_to_i, _ = initialize_helpers(data)
     # Custom shape to make indexing easier. We can pretend to work
     # with this shape while actually working with a 2d sparse matrix
-    shape = (4, 7, data.size, 4, 7, data.size)
-    G = sp.dok_matrix((4 * 7 * data.size, 4 * 7 * data.size), dtype=np.uint8)
+    shape = (4, 11, data.size, 4, 11, data.size)
+    G = sp.dok_matrix((4 * 11 * data.size, 4 * 11 * data.size), dtype=np.uint8)
 
     for i, j in product(range(len(data)), range(len(data[0]))):
         vertex = np.array((i, j))
@@ -20,18 +20,18 @@ def initialize_graph(data):
             # No U-turns
             if np.array_equal(direction, -prev_direction):
                 continue
-            for prev_momentum in range(7):
+            for prev_momentum in range(4, 11):
                 if np.array_equal(direction, prev_direction):
                     momentum = prev_momentum + 1
                 else:
-                    momentum = 0
+                    momentum = 4
 
-                # special case, must turn
-                if momentum > 6:
+                # special case, invalid
+                if momentum == 11:
                     continue
 
-                # special case must travel 4
-                if momentum == 0:
+                # special case, just turned must travel 4
+                if momentum == 4:
                     nbr = vertex + 4 * direction
                 else:
                     nbr = vertex + direction
@@ -51,33 +51,32 @@ def initialize_graph(data):
                     shape,
                     G.shape,
                 )
+
+                if l in [1, 3]:
+                    left, right = vertex, nbr
+                else:
+                    left, right = nbr, vertex
                 G[index] = (
-                    np.sum(data[vertex[0] : nbr[0] + 1, vertex[1] : nbr[1] + 1])
+                    np.sum(data[left[0] : right[0] + 1, left[1] : right[1] + 1])
                     - data[*vertex]
                 )
     return G
 
 
 def main():
-    with open("test3.txt", encoding="utf-8") as file:
+    with open("input.txt", encoding="utf-8") as file:
         data = np.array([list(map(int, line.rstrip())) for line in file.readlines()])
 
     print("initializing...")
-    # data = data[:10, :13]
     G = initialize_graph(data)
     print("solving...")
-    # starting_indices = list(range(3, 4 * 7 * data.size, data.size))
     dists = sp.csgraph.dijkstra(
         G,
-        indices=list(
-            range(0, 4 * 7 * data.size, data.size)
-        ),  # [0, 14 * data.size],  # North, 0 momentum, 0 and West, 0 momentum, 0
+        indices=list(range(0, 4 * 11 * data.size, data.size)),
         min_only=True,
         directed=True,
     )
-    for dist in set(dists[data.size - 1 :: data.size]):
-        print(dist)
-    print(min(dists[data.size - 1 :: data.size]))
+    print("ans", min(dists[data.size - 1 :: data.size]))
 
 
 if __name__ == "__main__":
